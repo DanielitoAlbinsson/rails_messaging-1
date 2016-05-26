addca7b2b555f00573edfccfda52b95cc6ca8259
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-05-26 14:31:17 +0200

    first rspec tests passing

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+    email "user@random.com"
+    password "MyString"
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(10) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  describe 'Fixtures' do
+    it 'should have valid Factory' do
+      expect(FactoryGirl.create(:user)).to be_valid
+    end
+  end
+
+  describe 'should not have an invalid email address' do
+    emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com', 'asdf@example', 'ddd@.d. .d', 'ddd@.d']
+    emails.each do |email|
+      it { is_expected.not_to allow_value(email).for(:email) }
+    end
+  end
+
+  describe 'should have a valid email address' do
+    emails = ['asdf@ds.com', 'hello@example.uk', 'test1234@yahoo.se', 'asdf@example.eu']
+    emails.each do |email|
+      it { is_expected.to allow_value(email).for(:email) }
+    end
+  end
+
+end
